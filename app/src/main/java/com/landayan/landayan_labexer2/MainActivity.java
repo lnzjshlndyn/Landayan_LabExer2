c755ddd51d63dd6132f5a05436446a04759b2914
lnzjshlndyn <2014069168@ust-ics.mygbiz.com>
2017-10-29 16:12:19 +0800

    initial commit

--- app/src/main/java/com/landayan/landayan_labexer2/MainActivity.java
+++ app/src/main/java/com/landayan/landayan_labexer2/MainActivity.java
@@ -1,19 +1,31 @@
 package com.landayan.landayan_labexer2;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText et_username, et_password;
+    Button btn_shared, btn_internal, btn_next;
+    SharedPreferences preferences;
+    FileOutputStream fos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText et_username, et_password;
-        Button btn_shared, btn_internal, btn_next;
 
         et_username = (EditText) findViewById(R.id.et_username);
         et_password = (EditText) findViewById(R.id.et_password);
@@ -21,5 +33,44 @@ public class MainActivity extends AppCompatActivity {
         btn_internal = (Button) findViewById(R.id.btn_internal);
         btn_next = (Button) findViewById(R.id.btn_next);
 
+        preferences = getSharedPreferences("preferences", Context.MODE_WORLD_READABLE);
     }
-}
+
+        public void Next(View view){
+            Intent intent = new Intent(this, SecondActivity.class);
+            startActivity(intent);
+            finish();
+
+        }
+
+        public void SharedPreferences(View view){
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putString("user", et_username.getText().toString());
+            editor.putString("pass", et_password.getText().toString());
+            editor.commit();
+
+            Toast.makeText(this, "Saved in Shared Preferences!", Toast.LENGTH_LONG).show();
+        }
+
+        public void InternalStorage(View view){
+            String user = et_username.getText().toString();
+            String pass = et_password.getText().toString();
+            String output = "Internal Storage: The username: " + user + ", and password: " + pass;
+
+            try{
+                fos = openFileOutput("output.txt", Context.MODE_PRIVATE);
+                fos.write(output.getBytes());
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally{
+                try{
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            Toast.makeText(this, "Saved in Internal Storage!", Toast.LENGTH_LONG).show();
+        }
+    }
--- app/src/main/java/com/landayan/landayan_labexer2/SecondActivity.java
+++ app/src/main/java/com/landayan/landayan_labexer2/SecondActivity.java
@@ -1,16 +1,70 @@
 package com.landayan.landayan_labexer2;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class SecondActivity extends AppCompatActivity {
+
+    Button btn_loadShared, btn_loadInternal, btn_clear, btn_back;
+    TextView tv_output;
+    SharedPreferences preferences;
+    FileInputStream fis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        Button btn_loadShared, btn_loadInternal, btn_clear, btn_back;
+        btn_loadShared = (Button) findViewById(R.id.btn_loadShared);
+        btn_loadInternal = (Button) findViewById(R.id.btn_loadInternal);
+        btn_clear = (Button)findViewById(R.id.btn_clear);
+        btn_back = (Button)findViewById(R.id.btn_back);
+        tv_output = (TextView)findViewById(R.id.tv_output);
+
+        preferences = getSharedPreferences("preference", Context.MODE_WORLD_READABLE);
+    }
+
+    public void LoadSharedPreferences(View view){
+        String user = preferences.getString("user", "");
+        String pass = preferences.getString("pass", "");
+        tv_output.setText("Shared Preferences - The Username: " + user + ", Password: " + pass);
+    }
+
+    public void LoadInternalStorage(View view){
+        StringBuffer buffer = new StringBuffer();
+        int read = 0;
+
+        try{
+            fis = openFileInput("output.txt");
+            while((read = fis.read()) != -1){
+                buffer.append((char)read);
+            }
+            fis.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        tv_output.setText(buffer.toString());
+    }
+
+    public void Clear(View view){
+        tv_output.setText("");
+    }
+
+    public void Back(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
